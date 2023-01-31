ce370c52532a73cb9d583543fb82db01b1d463b5
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2023-01-31 11:40:44 -0500

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.20"
-    id("org.jetbrains.compose") version "1.2.0"
+    kotlin("jvm") version "1.8.0"
+    id("org.jetbrains.compose") version "1.3.0"
     idea
 }
 
