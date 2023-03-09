00f6253921506d024252a2d4912b3cf8d001a311
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2023-03-09 15:34:38 -0500

    Upgrade dependencies
    Kotlin -> 1.8.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.8.10"
     id("org.jetbrains.compose") version "1.3.1"
     idea
 }
