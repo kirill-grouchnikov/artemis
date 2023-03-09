6a0f2b1c18bc8525be6620f721748797c42c1dc0
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2023-03-09 14:04:39 -0500

    Upgrade dependencies
    Compose -> 1.3.1
    Gradle -> 8.0.2
    Version checker -> 0.46.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.0"
-    id("org.jetbrains.compose") version "1.3.0"
+    id("org.jetbrains.compose") version "1.3.1"
     idea
 }
 
