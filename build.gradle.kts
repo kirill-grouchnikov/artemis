653238ec129477fd4800255ecba69df3be7304e4
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2026-09-08 12:35:47 -0400

    Upgrade dependencies
    Kotlin -> 2.4.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.4.0"
+    kotlin("jvm") version "2.4.20"
     alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.compose.compiler)
     idea
